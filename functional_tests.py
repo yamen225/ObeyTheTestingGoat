c7548c3c6df55a72aa2ef635f1f0a147c87a4833
yamen <yamen225@gmail.com>
2020-05-26 20:02:19 +0200

    Sending Post request, render view with new data, and updating functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -40,13 +40,18 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         # there's still a text box to add a new item
-
         # enters "Use peacock fweathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock fweathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2: Use peacock fweathers to make a fly',
+                      [row.text for row in rows])
+
         self.fail('Finish the test!')
         # page reloads and lists both items
 
--- lists/tests.py
+++ lists/tests.py
@@ -7,10 +7,11 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_post_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
