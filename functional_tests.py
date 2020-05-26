bdbb6f36628de7031158a01ed293a49cb0407fa9
yamen <yamen225@gmail.com>
2020-05-26 18:38:28 +0200

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
+import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,18 +21,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # notice page title mention to do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
         # Invited to enter a to do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Types "Buy peacock feathers" in a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # hits enter, the page lists
         # " 1: Buy peacock feathers" as an item
-
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         # there's still a text box to add a new item
 
         # enters "Use peacock fweathers to make a fly"
-
+        self.fail('Finish the test!')
         # page reloads and lists both items
 
         # Edith wonders whether the site will remember her list. Then she sees
