3076f7d3a681543cbc1d7426d1a0190104451ce0
yamen <yamen225@gmail.com>
2020-05-29 01:44:56 +0200

    Fix not adding secret key to env file

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -66,7 +66,7 @@ def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')
     append('.env', f'SITENAME={env.host}')
     current_contents = run('cat .env')
-    if 'DJANGO_DEBUG_FALSE' not in current_contents:
+    if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices(
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
