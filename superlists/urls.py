67928e53c67fa5206ce4136a63f208db25fd0a40
yamen <yamen225@gmail.com>
2020-05-28 04:08:44 +0200

    set STATIC_TOOT in settings and disable admin

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 from lists import views as list_views
 from lists import urls as list_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', list_views.home_page, name='home'),
     path('lists/', include(list_urls)),
 ]
