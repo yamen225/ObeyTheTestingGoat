773ba5728c9c928b7139c88c50ad17f5c12b0dd0
yamen <yamen225@gmail.com>
2020-05-29 02:24:17 +0200

    Fix wrong file names

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,30 +1,24 @@
+from unittest import skip
 from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
 MAX_WAIT = 10
 
 
-class LayoutAndStylingTest(FunctionalTest):
+class ItemValidationTest(FunctionalTest):
 
-    def test_layout_and_styling(self):
-        # Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
+    @skip
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
 
-        # She notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=100
-        )
-        # She starts a new list and sees the input is nicely
-        # centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=100
-        )
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+
+        # She tries again with some text for the item, which now works
+
+        # Perversely, she now decides to submit a second blank list item
+
+        # She receives a similar warning on the list page
+
+        # And she can correct it by filling some text in
+        self.fail('write me!')
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,24 +1,94 @@
-from unittest import skip
-from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 MAX_WAIT = 10
 
 
-class ItemValidationTest(FunctionalTest):
+class NewVisitorTest(FunctionalTest):
 
-    @skip
-    def test_cannot_add_empty_list_items(self):
-        # Edith goes to the home page and accidentally tries to submit
-        # an empty list item. She hits Enter on the empty input box
+    def test_can_start_a_list_for_one_user(self):
 
-        # The home page refreshes, and there is an error message saying
-        # that list items cannot be blank
+        # Edith checks her homepage
 
-        # She tries again with some text for the item, which now works
+        self.browser.get(self.live_server_url)
 
-        # Perversely, she now decides to submit a second blank list item
+        # notice page title mention to do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # She receives a similar warning on the list page
+        # Invited to enter a to do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-        # And she can correct it by filling some text in
-        self.fail('write me!')
+        # Types "Buy peacock feathers" in a text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        # hits enter, the page lists
+        # " 1: Buy peacock feathers" as an item
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        # there's still a text box to add a new item
+        # enters "Use peacock fweathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock fweathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table(
+            '2: Use peacock fweathers to make a fly')
+
+        # page reloads and lists both items
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique url
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now a new user, Francis, comes along to the site..
+        # We use a new browser session to make sure that no information
+        # of Edith's is coming through from coockies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He is less
+        # interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
