a41e1d12b44b5ed33f6afd957f032c73fbe34ae4
yamen <yamen225@gmail.com>
2020-06-08 21:56:13 +0200

    Adjust new list view to do model validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,8 +1,6 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
+from django.utils.html import escape
 
-from lists.views import home_page
 from lists.models import Item, List
 # Create your tests here.
 
@@ -58,11 +56,16 @@ class NewListTest(TestCase):
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
     def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/list/new', data={'item_text': ''})
+        response = self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = "You can't have an empty list item"
+        expected_error = escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
+
+    def test_invalid_list_item_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class NewItemTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from .models import Item, List
 
@@ -15,7 +16,15 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
+        # pass
     return redirect(f'/lists/{list_.id}/')
 
 
