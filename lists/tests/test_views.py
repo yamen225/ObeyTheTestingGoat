277273267390b64cd424877ca8eb799ffcd56912
yamen <yamen225@gmail.com>
2020-06-09 17:06:50 +0200

    Add unique ttext item by list constraint and validation with forms

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -100,7 +100,6 @@ class ListViewTest(TestCase):
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
