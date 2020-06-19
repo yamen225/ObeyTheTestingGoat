9b653ac5bd0820dcb19d209a7a60c24e60cbc025
yamen <yamen225@gmail.com>
2020-06-19 02:19:46 +0200

    url, placeholder view, and first-cut templates for my_lists

--- lists/urls.py
+++ lists/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('new', views.new_list, name='new_list'),
     path('<int:list_id>/', views.view_list,
          name='view_list'),
+    path('users/<str:email>/', views.my_lists, name='my_lists'),
 ]
